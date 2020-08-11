3e31e50ba6befc890c91a89d4d7c2a05773f6124
Benji Sidi <benjisidi@gmail.com>
2020-08-12 00:30:47 +0100

    added blueprintjs

---
+++ client/pages/_app.js
@@ -0,0 +1,6 @@
+import HomePage from "./index";
+import styles from "../index.css";
+
+export default function App() {
+  return <HomePage />;
+}
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,8 +1,16 @@
+import { Button } from "@blueprintjs/core";
+import { useState } from "react"
+
+
 const HomePage = () => {
+  const [counter, setCounter] = useState(1)
+  const incrementCounter = () => setCounter(counter + 1)
   return (
     <div>
       <span>Welcome to Next.js! </span>
       <a href="/about">Click here for the about page.</a>
+      Counter: {counter}
+      <Button intent="success" text="button content" onClick={incrementCounter} />
     </div>
   );
 };
