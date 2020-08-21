4553045ccc3a8c8fa6e5ec8c23e8b76f5db33681
VandalSausage <trumanwright13@gmail.com>
2020-08-21 17:07:48 +0100

    add auth routes to server

--- server.ts
+++ server.ts
@@ -2,17 +2,21 @@
 import express from "express";
 import { keys } from "./config";
 import mongoose from "mongoose";
-import { users } from "./routes/api";
+import { users, auth } from "./routes/api";
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 const main = async () => {
+  app.use(express.json());
   app.use("/api/users", users);
+  app.use("/api/auth", auth);
   try {
     await mongoose.connect(keys.mongoURI, {
       useCreateIndex: true,
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      useFindAndModify: false,
     });
     console.log("Connected to MongoDB");
   } catch (e) {
