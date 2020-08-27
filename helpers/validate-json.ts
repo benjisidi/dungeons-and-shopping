fbcd52b7505b608c1987a41fb7a0c89ae6c6e9eb
VandalSausage <trumanwright13@gmail.com>
2020-08-27 18:03:49 +0100

    add functions for validating item and stock arrays

--- helpers/validate-json.ts
+++ helpers/validate-json.ts
@@ -1,4 +1,6 @@
 import difference from "lodash/difference";
+import { Item, Stock } from "../types";
+import { isArray } from "lodash";
 
 export const getMissingKeys = (
   requiredKeys: string[],
@@ -10,5 +12,70 @@ export const getMissingKeys = (
   return {
     missingKeys: missingKeys.length && missingKeys,
     wrongKeys: wrongKeys.length && wrongKeys,
+  };
+};
+
+const itemKeys = ["name"];
+
+declare interface PossibleItem extends Partial<Item> {
+  [key: string]: unknown;
+}
+
+export const validateItemsArray = (
+  items: PossibleItem[],
+  exact = true,
+  id = false
+): { rejectedItems: unknown[]; validItems?: Partial<Item>[] } => {
+  if (!isArray(items)) {
+    return { rejectedItems: null };
+  }
+  const keysToCheck = id ? itemKeys.concat(["_id"]) : itemKeys;
+  const rejectedItems = [];
+  const validItems = [];
+  items.forEach((item) => {
+    const { missingKeys, wrongKeys } = getMissingKeys(keysToCheck, item);
+    if (
+      (missingKeys && exact) ||
+      wrongKeys ||
+      (id && missingKeys && missingKeys.includes("_id"))
+    ) {
+      rejectedItems.push(item);
+    } else {
+      validItems.push(item);
+    }
+  });
+
+  return {
+    validItems: validItems.length && validItems,
+    rejectedItems: rejectedItems.length && rejectedItems,
+  };
+};
+
+declare interface PossibleStock extends Partial<Stock> {
+  [key: string]: unknown;
+}
+export const validateStockArray = (
+  stock: PossibleStock[]
+): {
+  rejectedStock: unknown[];
+  validStock?: { number: number; itemId: string }[];
+} => {
+  if (!isArray(stock)) {
+    return { rejectedStock: null };
+  }
+
+  const rejectedStock = [];
+  const validStock = [];
+  stock.forEach(({ itemId, number, ...anythingElse }) => {
+    if (itemId && number) {
+      validStock.push({ itemId, number });
+    } else {
+      rejectedStock.push({ itemId, number, ...anythingElse });
+    }
+  });
+
+  return {
+    validStock: validStock.length && validStock,
+    rejectedStock: rejectedStock.length && rejectedStock,
   };
 };
