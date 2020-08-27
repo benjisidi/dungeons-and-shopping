4dde251f95127f0188b2f199cc2b451ceea24f0f
VandalSausage <trumanwright13@gmail.com>
2020-08-27 18:01:59 +0100

    add global and stock routes to server

--- server.ts
+++ server.ts
@@ -1,18 +1,21 @@
-// Boilerplate to test babel/TS
 import express from "express";
 import { keys } from "./config";
 import mongoose from "mongoose";
-import { users, auth, shops, admin } from "./routes/api";
+import { stock, users, auth, shops, admin, items, global } from "./routes/api";
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 const main = async () => {
-  app.use(express.json());
-  app.use("/api/users", users);
-  app.use("/api/auth", auth);
-  app.use("/api/shops", shops);
-  app.use("/api/admin", admin);
+  app
+    .use(express.json())
+    .use("/api/users", users)
+    .use("/api/auth", auth)
+    .use("/api/shops", shops)
+    .use("/api/admin", admin)
+    .use("/api/items", items)
+    .use("/api/stock", stock)
+    .use("/api/global/items", global);
   try {
     await mongoose.connect(keys.mongoURI, {
       useCreateIndex: true,
