fa557ab57259989e924aa585651e15fc84e25017
Benji Sidi <benjisidi@gmail.com>
2020-09-23 11:27:11 +0100

    Replaced NextJS with manual react+webpack. Added basic react-router implementation Added basic blueprintjs implementation Added reactn and react-query

---
+++ client/src/components/App.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Button, Alert, Intent } from "@blueprintjs/core";
+
+export const App = () => {
+  const [alert, setAlert] = useState(false);
+  return (
+    <Router>
+      <Switch>
+        <Route path="/about">
+          <h1>
+            Hello, World! Click here for <Link to="/">Home</Link>
+          </h1>
+          <Button onClick={() => setAlert(true)}>Click me!</Button>
+          <Alert
+            cancelButtonText="Cancel"
+            confirmButtonText="Cool brah"
+            intent={Intent.SUCCESS}
+            isOpen={alert}
+            onClose={() => setAlert(false)}
+          >
+            You clicked da button
+          </Alert>
+        </Route>
+        <Route path="/">
+          <h1>
+            Hello, World! Click here for <Link to="/about">About</Link>
+          </h1>
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
