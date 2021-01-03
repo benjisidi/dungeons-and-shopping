5d17fa62ee5469523e98621f6a67cf778f184db6
VandalSausage <trumanwright13@gmail.com>
2021-01-03 17:21:28 +0000

    create a form for purchasing stock and associated reducer methods

--- client/src/components/stock/purchase-form.tsx
+++ client/src/components/stock/purchase-form.tsx
@@ -1,3 +1,72 @@
+import { Button, Intent, NumericInput } from "@blueprintjs/core";
 import React from "react";
+import { useForm } from "react-hook-form";
+import type { PageState, StockAction } from "src/types";
+import styled from "styled-components";
 
-export const PurchaseForm = () => <div />;
+import { AppToaster } from "../../common";
+import { ErrorText, FormWrapper } from "../shared";
+
+const Input = styled(NumericInput)`
+  margin-bottom: 5px;
+`;
+
+export const PurchaseForm = ({
+  state,
+  dispatch,
+}: {
+  state: PageState;
+  dispatch: React.Dispatch<StockAction>;
+}) => {
+  const {
+    register,
+    handleSubmit,
+    errors,
+    watch,
+    setValue,
+    trigger,
+  } = useForm();
+  const { number } = state.stock[state.purchaseItem as string];
+  const submit = (values: { quantity: number }) => {
+    dispatch({ type: "ADD_TO_CART", payload: values.quantity });
+    return AppToaster.show({
+      message: `Successfully added ${values.quantity} ${
+        values.quantity === 1 ? "item" : "items"
+      } to cart!`,
+      intent: Intent.SUCCESS,
+      icon: "tick-circle",
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit(submit)}>
+      <FormWrapper>
+        <Input
+          defaultValue={0}
+          intent={errors.quantity ? Intent.DANGER : "none"}
+          inputRef={register({
+            required: "you must choose a quantity",
+            valueAsNumber: true,
+            min: { value: 0, message: "you must choose at least 1!" },
+            max: {
+              value: number,
+              message: "there aren't that many available!",
+            },
+          })}
+          onValueChange={(val) => {
+            setValue("quantity", val);
+            return trigger("quantity");
+          }}
+          name="quantity"
+          placeholder="How many?"
+          min={0}
+          max={number}
+        />
+        <ErrorText>{errors.quantity?.message}</ErrorText>
+        <Button disabled={watch("quantity") === 0} type="submit">
+          Add To Cart
+        </Button>
+      </FormWrapper>
+    </form>
+  );
+};
--- client/src/components/stock/reducer.ts
+++ client/src/components/stock/reducer.ts
@@ -1,21 +1,58 @@
 import cloneDeep from "lodash/cloneDeep";
 
-import type { Details, PageState, StockAction } from "../../types";
+import type { Item, PageState, Stock, StockAction } from "../../types";
 
-const openDetailsOverlay = (
-  state: PageState,
-  details: Details,
-  name: string
-): PageState => {
+const openDetailsOverlay = (state: PageState, itemId: string): PageState => {
   const newState = cloneDeep(state);
-  newState.detailsState = { details, name };
+  newState.detailsItem = itemId;
   newState.isDetailsOpen = true;
   return newState;
 };
 const closeDetailsOverlay = (state: PageState): PageState => {
   const newState = cloneDeep(state);
-  newState.detailsState = null;
+  newState.detailsItem = null;
   newState.isDetailsOpen = false;
+  return newState;
+};
+const openPurchaseForm = (
+  state: PageState,
+  purchaseItem: PageState["purchaseItem"]
+): PageState => {
+  const newState = cloneDeep(state);
+  newState.purchaseItem = purchaseItem;
+  newState.isPurchaseFormOpen = true;
+  return newState;
+};
+const addToCart = (state: PageState, number: number): PageState => {
+  const newState = cloneDeep(state);
+  const purchaseId = newState.purchaseItem as string;
+  const existingCartEntry = newState.cart[purchaseId];
+  const stockEntry = newState.stock[purchaseId] as Stock & Item;
+  stockEntry.number -= number;
+  if (!existingCartEntry) {
+    newState.cart[purchaseId] = { itemId: purchaseId, number };
+  } else {
+    existingCartEntry.number += number;
+  }
+  newState.purchaseItem = null;
+  newState.isPurchaseFormOpen = false;
+  return newState;
+};
+
+const cancelPurchase = (state: PageState): PageState => {
+  const newState = cloneDeep(state);
+  newState.purchaseItem = null;
+  newState.isPurchaseFormOpen = false;
+  return newState;
+};
+const openCart = (state: PageState): PageState => {
+  const newState = cloneDeep(state);
+  newState.isCartOpen = true;
+  return newState;
+};
+const closeCart = (state: PageState): PageState => {
+  const newState = cloneDeep(state);
+  newState.isCartOpen = false;
   return newState;
 };
 
@@ -25,10 +62,22 @@ export const stockReducer = (
 ): PageState => {
   switch (action.type) {
     case "OPEN_DETAILS":
-      return openDetailsOverlay(state, action.payload, action.meta);
+      return openDetailsOverlay(state, action.payload);
     case "CLOSE_DETAILS":
       return closeDetailsOverlay(state);
+    case "OPEN_PURCHASE":
+      return openPurchaseForm(state, action.payload);
+    case "ADD_TO_CART":
+      return addToCart(state, action.payload);
+    case "CANCEL_PURCHASE":
+      return cancelPurchase(state);
+    case "CLOSE_CART":
+      return closeCart(state);
+    case "OPEN_CART":
+      return openCart(state);
     default:
-      throw new Error("stock action type not recognised");
+      throw new Error(
+        `${(action as StockAction)?.type} is not a recognised stock action type`
+      );
   }
 };
